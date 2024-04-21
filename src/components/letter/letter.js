9b202fda011edfd259475506e9144f3efdf67ad0
Birru Vamshi <83230026+Vamshi-Birru@users.noreply.github.com>
2024-04-21 14:53:48 +0530

    Add files via upload

---
+++ src/components/letter/letter.js
@@ -0,0 +1,50 @@
+import React, { useState, useRef } from 'react';
+import './letter.css';
+import audioFile from './valimai.mp3';
+
+const Letter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        Oh mummy🖤,<br />
+        Tinnava😋....<br />nik istamaina pata pedudham anukunna but, just now realised that, i didn't even know that song😿<br />
+       forgive me🥺 
+       <br/> 
+       always yours,<br />
+        @vamshiiiiii
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default Letter;
